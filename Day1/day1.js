a0223c822de58625856667dd362abb16722563b8
NazioLT <zoupeurletroll@gmail.com>
2024-12-18 14:11:47 +0100

    update

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,1 +1,30 @@
-alert("tt")
+function readText(text) {
+    const array = text.split(/[\s\n]+/);
+    let pair = true;
+
+    let list1, list2;
+    array.forEach(element => {
+        list2.push(element);
+        pair = !pair;
+    });
+
+    list1.sort();
+    list2.sort();
+
+    let totalDistance = 0;
+    for (let i = 0; i < list1.length; i++) {
+        const element1 = list1[i];
+        const element2 = list2[i];
+
+        totalDistance += Math.abs(element1 - element2);
+    }
+
+    alert(totalDistance);
+}
+
+fetch("Day1Input.txt")
+    .then((res) => res.text())
+    .then((text) => {
+        readText(text);
+    })
+    .catch((e) => console.error(e));
